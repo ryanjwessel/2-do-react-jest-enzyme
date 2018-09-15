0425ba5ad685f2928046e3d92881aaddd13f9b06
Ryan Wessel <rynjwssl@gmail.com>
2018-09-15 13:01:13 -0400

    remove redux from project, as I want to emphasize Jest for testing instead. One step at a time! Also, give it a cool name :)

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+// STYLES
+import './styles/main.scss';
+
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -8,7 +11,22 @@ class App extends Component {
 
 	render() {
 		return(
-			<p>Hello world!</p>
+			<div className="container-fluid container">
+				<div className="row center-xs">
+					<div className="col-xs-12">
+						<h1>2-DO</h1>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<input type="text" placeholder="Get it done!" />
+					</div>
+					<div className="col-xs-12">
+						<button type="button">Add 2-DO</button>
+					</div>
+				</div>
+				<hr />
+			</div>
 		);
 	}
 }
